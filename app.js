7f49dae05310640cb9c9f249231217d9a8961ae7
aimnsyd <aimnsyd00@gmail.com>
2025-06-27 22:07:40 +0800

    Update

--- app.js
+++ app.js
@@ -53,7 +53,38 @@ class App{
 		this.loadingBar = new LoadingBar();
 
         this.loadAmbientSound();
-		
+
+        // Add Canvas UI with toggle button
+        const config = {
+            panelSize: { height: 0.3 },
+            height: 128,
+            toggleSound: { position: { top: 20 }, height: 50, fontSize: 40, backgroundColor: "#333", fontColor: "#fff" }
+        };
+
+        const content = {
+            toggleSound: "🔈 Sound On"
+        };
+
+        this.ui = new CanvasUI(content, config);
+        this.ui.mesh.position.set(0, 1.5, -2);
+        this.scene.add(this.ui.mesh);
+
+        this.soundMuted = false;
+        this.ui.updateElement("toggleSound", "🔈 Sound On");
+        this.ui.update();
+
+        this.ui.update = () => {
+            this.ui.updateElement("toggleSound", this.soundMuted ? "🔇 Muted" : "🔈 Sound On");
+        };
+
+        this.ui.element.addEventListener("click", () => {
+            this.soundMuted = !this.soundMuted;
+            if (this.ambientSound) {
+                this.ambientSound.setVolume(this.soundMuted ? 0 : 0.5);
+            }
+            this.ui.update();
+        });
+
 		this.loadCollege();
 		
         this.immersive = false;
