5cb7d00565634bef47178301b7d960700025a5a9
aimnsyd <aimnsyd00@gmail.com>
2025-06-27 22:40:17 +0800

    remove space

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 import * as THREE from './libs/three/three.module.js';
 import { GLTFLoader } from './libs/three/jsm/GLTFLoader.js';
 import { DRACOLoader } from './libs/three/jsm/DRACOLoader.js';
